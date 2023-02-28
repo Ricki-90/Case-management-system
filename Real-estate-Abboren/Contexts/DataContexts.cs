a789eea903649f4287cc57329f964cfcba7f596d
Berrr <Berrr@JARLEN>
2023-02-28 18:52:43 +0100

    Add project files.

---
+++ Real-estate-Abboren/Contexts/DataContexts.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Real_estate_Abboren.Contexts;
+
+internal class DataContexts : DbContext
+{
+    public DataContexts(DbContextOptions<DataContexts> options) : base(options)
+    {
+    }
+}
