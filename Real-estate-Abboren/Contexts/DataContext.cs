84249ec515be337a8bfdfc9f3a7db49f2b0707c8
Berrr <Berrr@JARLEN>
2023-03-01 18:58:43 +0100

    Created Entitites

---
+++ Real-estate-Abboren/Contexts/DataContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Real_estate_Abboren.Contexts;
+
+internal class DataContext : DbContext
+{
+    private readonly string _connectionstring = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Berrr\Desktop\Case-management-system\Case-management-system\Real-estate-Abboren\Contexts\sql_db.mdf;Integrated Security=True;Connect Timeout=30";
+
+    #region constructors
+    public DataContext()
+    {
+    }
+
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+    #endregion
+
+    #region overrides
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+            optionsBuilder.UseSqlServer(_connectionstring);
+    }
+
+    #endregion
+
+    #region entitites
+
+    #endregion
+}
---
+++ Real-estate-Abboren/Models/Entities/AddressEntity.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Real_estate_Abboren.Models.Entities;
+
+
+internal class Case
+{
+    [Key]
+    public int CaseId { get; set; }
+
+
+}
+
+
+
+
+internal class AddressEntity
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [Column(TypeName = "nvarchar(50)")]
+    public string StreetName { get; set; } = string.Empty;
+
+    [Required]
+    [Column(TypeName = "char(6)")]
+    public string PostalCode { get; set; } = string.Empty;
+
+    [Required]
+    [Column(TypeName = "nvarchar(50)")]
+    public string City { get; set; } = string.Empty;
+
+    public ICollection<RenterEntity> Renters = new HashSet<RenterEntity>();
+}
+
+
+internal class RenterEntity
+{
+    [Key]
+    public int RenterId { get; set; }
+
+    [StringLength(50)]
+    public string FirstName { get; set; } = string.Empty;
+
+    [StringLength(50)]
+    public string LastName { get; set; } = string.Empty;
+
+    [StringLength(100)]
+    public string Email { get; set; } = string.Empty;
+
+    [Column(TypeName = "char(13)")]
+    public string? PhoneNumber { get; set; }
+    
+    public int AddressId {get; set; }
+    public AddressEntity Address { get; set; } = null!;
+}
